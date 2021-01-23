6d9785b388112d65957779c8692474b85b312e40
adm-chernyshev <an.chernyshev@corp.mail.ru>
2021-01-23 15:03:42 +0300

    added passcodevc and local authentication

---
+++ PswrdStore/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PswrdStore
+//
+//  Created by an.chernyshev on 23/01/2021.
+//  Copyright © 2021 a.dm.chernyshev. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .orange
+    }
+
+
+}
+
